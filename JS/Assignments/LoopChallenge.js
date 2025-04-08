c89b7ab896c76155bb1a44b47fc231b9c746bab5
Maulik Gajipara <55870961+gajiparamaulik@users.noreply.github.com>
2025-04-08 08:08:25 +0530

    7th issue

--- JS/Assignments/LoopChallenge.js
+++ JS/Assignments/LoopChallenge.js
@@ -130,3 +130,25 @@ function countWorkingDays(days) {
 // You're shopping for groceries and want to know the total cost of all the items in your cart. You have an array of item prices.
 // Problem Statement:
 // Create a function that takes an array of item prices and returns the total cost.
+
+
+
+
+// 09. JS Loops
+// You've been tasked with designing the perfect shiny diamond rug cursh's room. They love stars, and you've decided to make a 
+// sparkling pattern using them. You need to help them create this rug, where the stars are arranged in the shape of a diamond! ✰✰
+// Problem Statement:
+// Create a function shinyDiamondRug(n) that prints a shiny diamond shape made of stars. The number n represents the size of the 
+// diamond, with the middle of the diamond having 2n - 1 stars.
+// Important Rules:
+// 1 Each line must not have trailing spaces.
+// 2 The output must not have an extra newline at the end.
+// For example:
+// • For n = 4, the output will look like:
+// *
+// ***
+// *****
+// *******
+// *****
+// ***
+// *
