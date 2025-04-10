0ef82fb77e8f29ad7452d30f45c0c005ca1e081e
Maulik Gajipara <55870961+gajiparamaulik@users.noreply.github.com>
2025-04-10 08:05:14 +0530

    start array question

---
+++ JS/Assignments/ArrayChallenge.js
@@ -0,0 +1,8 @@
+
+// 01. JS Arrays
+// Easy
+// You are organizing a party and have a list of invited guests. But just before the party starts, a new friend decides to join. 
+// You need to add them to the guest list.
+// Problem Statement: Create a function that adds a new guest list and returns the updated list.
+// Hint 1
+// Use the method that allows you to add an item at the end of an array.
