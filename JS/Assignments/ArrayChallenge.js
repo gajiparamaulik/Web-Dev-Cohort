9b686da079489dc3adc2bc2edf9a07fe6cc64049
Maulik Gajipara <55870961+gajiparamaulik@users.noreply.github.com>
2025-04-11 08:20:27 +0530

    js array 2 solved

--- JS/Assignments/ArrayChallenge.js
+++ JS/Assignments/ArrayChallenge.js
@@ -7,8 +7,22 @@
 // Hint 1
 // Use the method that allows you to add an item at the end of an array.
 
-
 function addGuest(guestList, newGuest) {
     guestList.push(newGuest);
     return guestList;
+}
+
+
+
+
+// 02. JS Arrays
+// Easy
+// You have a jar full of candies, but your little sibling keeps eating the last one! Your job is to remove the last candy from the jar.
+// Problem Statement: 
+// Create a function that removes the last candy from the jar and returns the updated jar.
+
+
+function eatCandy(candyJar) {
+    candyJar.pop();
+    return candyJar;
 }
