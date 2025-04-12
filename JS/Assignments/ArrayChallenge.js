79da39b3d3b3f2fe5aa64db40ad37644854953ec
Maulik Gajipara <55870961+gajiparamaulik@users.noreply.github.com>
2025-04-12 23:01:47 +0530

    3rd array question

--- JS/Assignments/ArrayChallenge.js
+++ JS/Assignments/ArrayChallenge.js
@@ -26,3 +26,15 @@ function eatCandy(candyJar) {
     candyJar.pop();
     return candyJar;
 }
+
+
+
+
+// 03. JS Arrays
+// Easy Hints
+// A puppy named Max was playing in the park, but he went to the first position of the queue instead of waiting at the end. You need to add Max at the beginning of the queue.
+// Problem Statement: Create a function that adds a puppy to the front of the queue and returns the updated queue.
+// Hint 1
+// Think of a method that adds an item to the beginning of an array.
+
+
