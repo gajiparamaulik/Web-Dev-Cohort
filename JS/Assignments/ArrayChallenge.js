65194a257e7dcc84a5e56ee3893479e34386e100
Maulik Gajipara <55870961+gajiparamaulik@users.noreply.github.com>
2025-04-14 12:09:04 +0530

    4th array ques solved

--- JS/Assignments/ArrayChallenge.js
+++ JS/Assignments/ArrayChallenge.js
@@ -48,3 +48,9 @@ function addPuppy(queue, puppyName) {
 // Easy Hints
 // A school bus has students inside, but the first student in line needs to get off at the next stop. Remove the first student from the bus.
 // Problem Statement: Create a function that removes the first student from the bus and returns the updated list.
+
+
+function removeStudent(bus) {
+    bus.shift(); 
+    return bus;
+}
