d941a641d68c22e30b3884453e3a6dc352111e85
Maulik Gajipara <55870961+gajiparamaulik@users.noreply.github.com>
2025-04-15 20:23:01 +0530

    6th array question

--- JS/Assignments/ArrayChallenge.js
+++ JS/Assignments/ArrayChallenge.js
@@ -67,3 +67,10 @@ function removeStudent(bus) {
 
 
 
+
+// 06. JS Arrays
+// Easy
+// You have a list of food items, but you want to keep only the healthy ones. Remove all items that contain "Burger".
+// Problem statement: Create a function that removes unhealthy food items (those containing "Burger") and returns the updated list.
+
+
