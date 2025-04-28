118586b79f6a5708200e101d6a57f615dba09d25
Maulik Gajipara <55870961+gajiparamaulik@users.noreply.github.com>
2025-04-28 22:14:51 +0530

    loop 10th question

--- JS/Assignments/LoopChallenge.js
+++ JS/Assignments/LoopChallenge.js
@@ -152,3 +152,24 @@ function countWorkingDays(days) {
 // *****
 // ***
 // *
+
+
+
+
+// 10. JS Loops
+// Easy
+// You're on a camping trip with your friends, and you all want to set up a starry mountain as a symbol of your adventure. 
+// To make things more fun, you decide to use stars to draw an inverted mountain pattern. The challenge: 
+// print it so everyone can admire the majestic mountain shape!
+// Problem Statement:
+// Create a function invertedMountain(n) that prints an inverted mountain made of stars. The number n represents the number of 
+// stars at the top, and the pattern should reduce by one star each line until only one star is left at the bottom.
+// For example:
+// • For n = 4, the output will look like:
+// ****
+// ***
+// **
+// *
+// Hint 1
+// When you want to print multiple lines, you can use '\n' to add a new line after each row of stars. 
+// This will help you keep the output organized and align it properly!
