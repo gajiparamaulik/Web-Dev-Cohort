c918d6d95a1c3c6a9128898d78001e3a060556ce
Maulik Gajipara <55870961+gajiparamaulik@users.noreply.github.com>
2025-05-20 20:06:01 +0530

    Prototype ques2

--- JS/Assignments/Prototype.js
+++ JS/Assignments/Prototype.js
@@ -7,3 +7,15 @@
 // Challenge
 // • Implement a constructor function Animal that initializes the name property.
 // • Attach a method makeSound to its prototype that returns "Some generic sound".
+
+
+
+
+// 02. JS Prototype
+// Easy **Hints
+// Problem statement
+// You are designing a simple robot system. Each robot has a name and a batteryLevel. The robot should have a method charge() 
+// that increases the battery level by 20, but it cannot exceed 100.
+// Challenge
+// • Implement a constructor function Robot that initializes name and batteryLevel.
+// • Attach a method charge() to its prototype that increases batteryLevel by 20, ensuring it does not go above 100.
