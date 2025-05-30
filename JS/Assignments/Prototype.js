8a74a74f0418c77cee90578d02922ffa531368df
Maulik Gajipara <55870961+gajiparamaulik@users.noreply.github.com>
2025-05-30 22:13:08 +0530

    worked on prototype 9

--- JS/Assignments/Prototype.js
+++ JS/Assignments/Prototype.js
@@ -105,3 +105,18 @@
 // Challenge
 // • Implement Library constructor with a books array.
 // • Attach addBook (book) and findBook (title) methods to the prototype.
+
+
+
+
+// 09. JS Prototype
+// Hard -Hints
+// Problem statement
+// You are building a banking system where multiple bank accounts can exist. Each account has an accountNumber, holderName, and balance. 
+// Implement the following methods:
+// • deposit (amount): Adds money to the balance.
+// • withdraw(amount): Deducts money but prevents overdraft.
+// • transfer (amount, targetAccount): Transfers money from one account to another if the balance allows it.
+// Challenge
+// • Implement BankAccount constructor with accountNumber, holderName, and balance.
+// • Attach deposit (amount), withdraw(amount), and transfer (amount, targetAccount) methods to the prototype.
