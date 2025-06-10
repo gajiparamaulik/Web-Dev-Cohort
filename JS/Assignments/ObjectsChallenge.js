1272801de73cd480dba44298814b7f267267b38c
Maulik Gajipara <55870961+gajiparamaulik@users.noreply.github.com>
2025-06-10 08:25:49 +0530

    2nd objects ques

--- JS/Assignments/ObjectsChallenge.js
+++ JS/Assignments/ObjectsChallenge.js
@@ -9,3 +9,16 @@
 // • age should be a positive integer greater than 5. 
 // • grade should be a string like "10th", "12th", etc.
 // • return "Invalid input" for wrong inputs.
+
+
+
+
+// 02. JS Objects
+// Easy Statement
+// You are developing a car rental service. Each car has a brand and model, but some cars don't have a color assigned yet. 
+// You need to add a color property dynamically when a customer selects a car.
+// Challenge
+// Write a function that takes a car object and a color string, then adds the color property to the object.
+// Constraints
+// ⚫ car should be a valid object with at least brand and model properties.
+// ⚫ color should be a non-empty string, otherwise return "Invalid color".
