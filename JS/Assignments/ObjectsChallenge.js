e07464c673a5aa9f1e88672fe82e99516af9ed40
Maulik Gajipara <55870961+gajiparamaulik@users.noreply.github.com>
2025-06-11 21:15:05 +0530

    4th objects ques

--- JS/Assignments/ObjectsChallenge.js
+++ JS/Assignments/ObjectsChallenge.js
@@ -34,3 +34,16 @@
 // Write a function that checks if a product object has a discount property and returns true or false.
 // Constraints
 // product should be a valid object.
+
+
+
+
+// 04. JS Objects
+// Easy Statement
+// For security reasons, when a user logs out, their password should be removed from the user object before storing it in logs 
+// or analytics.
+// Challenge
+// Write a function that removes the password property from a user object and returns the updated object.
+// Constraints
+// ⚫ user should be a valid object with at least a username and password.
+// • If password does not exist, return the object as it is.
