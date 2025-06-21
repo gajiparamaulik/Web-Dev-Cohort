b3b7b752486731751561429dd95a8c91732e4a0d
Maulik Gajipara <55870961+gajiparamaulik@users.noreply.github.com>
2025-06-21 23:22:11 +0530

    added 10th object question

--- JS/Assignments/ObjectsChallenge.js
+++ JS/Assignments/ObjectsChallenge.js
@@ -116,3 +116,20 @@
 // • The input should be a valid object.
 // • The function should work with nested objects and arrays inside objects.
 // • The function should not modify the original object
+
+
+
+
+// 10. JS Objects
+// Medium Statement
+// In many applications, data is stored in deeply nested objects. Accessing a property from a deeply nested object requires 
+// multiple checks to avoid errors. Instead of manually checking each level, we need a function that safely retrieves a value from 
+// a nested object using a dot-separated key path.
+// Challenge
+// Write a function that takes a nested object and a key path string (e.g., "user.address.city") and returns the value. If any part 
+// of the path is missing, return "Key not found".
+// Constraints
+// • The input object should be valid.
+// • The key path should be a string with dot notation (.) separating keys.
+// • If a key is missing, return "Key not found".
+// • The function should handle deeply nested objects.
