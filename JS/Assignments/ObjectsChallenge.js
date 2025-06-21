b5154b6b81c50cb9493956a6a09cadc3febb23bc
Maulik Gajipara <55870961+gajiparamaulik@users.noreply.github.com>
2025-06-21 23:20:46 +0530

    added 9th object

--- JS/Assignments/ObjectsChallenge.js
+++ JS/Assignments/ObjectsChallenge.js
@@ -104,3 +104,15 @@
 
 
 
+
+// 09. JS Objects
+// Hard Statement
+// You are working on a web application where objects contain nested properties. JavaScript's built-in assignment (=) creates a 
+// shallow copy, meaning changes to the copied object will also affect the original. To prevent this, you need to create a deep copy 
+// of an object, ensuring that nested objects are also cloned properly.
+// Challenge
+// Write a function that takes an object and returns a deep copy of it.
+// Constraints
+// • The input should be a valid object.
+// • The function should work with nested objects and arrays inside objects.
+// • The function should not modify the original object
