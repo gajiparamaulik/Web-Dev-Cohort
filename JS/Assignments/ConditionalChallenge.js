3b72781c764bc689ba960aba7988344f4e269e0b
Maulik Gajipara <55870961+gajiparamaulik@users.noreply.github.com>
2025-07-03 21:36:26 +0530

    operator ques solved

--- JS/Assignments/ConditionalChallenge.js
+++ JS/Assignments/ConditionalChallenge.js
@@ -175,18 +175,21 @@ function convertTemperature(value, scale) {
 // Problem Statement:
 // Write a function that uses switch-case to perform arithmetic operations. Handle the edge case of "Cannot divide by zero".
 
-
 function calculator(num1, num2, operator) {
-    switch (operator) { 
-        case "+":
-            return num1 + num2;
-        case "-":
-            return num1 - num2;
-        case "*":
-            return num1 *num2;
-        case "/":
-            return num2 !== 0 ? num1 / num2: "Cannot divide by zero";
-        default:
-            return "Invalid Operator";
-    }
+  switch(operator) {
+    case "+":
+    return num1 + num2;
+
+    case "-":
+    return num1 - num2;
+
+    case "*":
+    return num1 * num2;
+
+    case "/":
+    return num1 !== 0 ? num1/num2 : "Cannot divide by zero";
+
+    default:
+    return "Invalid Operator";
+  }
 }
