498d9ceb1198457528697bc9dd6b17eb7bb50a4f
Maulik Gajipara <55870961+gajiparamaulik@users.noreply.github.com>
2025-09-06 22:34:54 +0530

    js array question

--- JS/Assignments/ArrayChallenge.js
+++ JS/Assignments/ArrayChallenge.js
@@ -93,4 +93,3 @@ function removeStudent(bus) {
 // You and your friends are making a list of movies to watch. You want to check how many movies are in the list.
 // Problem Statement: Create a function that returns the number of movies in the movie list.
 
-
