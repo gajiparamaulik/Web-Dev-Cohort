4649a21e5467477958859d957d0101d0919a1097
maulik <gajiparamaulik@gmail.com>
2026-03-08 16:16:36 +0530

    8thfeb practice done

--- JS/Lecture/8thFeb.js
+++ JS/Lecture/8thFeb.js
@@ -1,3 +1,67 @@
-// Problem Create an 
+// Problem: Create an array containing different types of teas.
+const teas = ["Grean Tea", "Black Tea", "Oolang Tea", "White Tea", "Herbal Tea"];
+console.log(teas);
 
-// 8:37 video recording check, 9:48
+
+// Problem: Add "Ginger Tea" to the existing list of teas.
+teas.push("Ginger Tea");
+
+
+// Problem: Remove "Oolang Tea" from the list of teas.
+const index = teas.indexOf("Oolang Tea");
+console.log(index);
+if(index > -1) {
+    teas.splice(index, 1);
+}
+
+
+// Problem: Filter the list to only include teas that are caffeinated.
+const filteredTeas = teas.filter((tea) => tea !== 'Herbal Tea');
+console.log("filteredTeas :", filteredTeas);
+
+
+// Problem: Sort the list of teas in alphabetical order.
+const sorting = teas.sort();
+console.log("sort : ", sorting);
+
+
+// Problem: use a for loop to print each type of tea in the array
+for(let i = 0; i < teas.length; i++) {
+    console.log(teas[i]);
+}
+
+
+// Problem: Use a for loop to count how many teas are caffeinated (excluding "Herbal Tea");
+let caffeinatedMyTeas = 0;
+for(let i = 0; i < teas.length; i++) {
+    if(teas[i] !== "Herbal Tea") {
+        caffeinatedMyTeas++;
+    }
+}
+console.log(caffeinatedMyTeas);
+
+
+// Problem: Use a for loop to create a new array with all tea name in Uppercase.
+const uppercaseTeas = [];
+for(let i = 0; i < teas.length; i++) {
+    uppercaseTeas.push(teas[i].toUpperCase());
+} 
+console.log(uppercaseTeas);
+
+
+// Problem: Use a for loop to find the tea name with the most character.
+let longestChar = "";
+for(let i = 0; i < teas.length; i++) {
+    if(teas[i].length > longestChar.length) {
+        longestChar = teas[i];
+    }
+}
+console.log(longestChar);
+
+
+// Problem: Use a for loop to reverse the order of teas in the array.
+const reverseArray = [];
+for(let i = teas.length - 1; i >= 0; i--) {
+    reverseArray.push(teas[i]);
+}
+console.log(reverseArray);
